28c080fd69b6fd67dd8a4e0f6f0eeba16bee829b
Evgeny Zimin <evgeny@zimin.ca>
2020-08-30 02:22:09 -0400

    just another Math transforming from int to bits

--- reverse-bits/src/Solution.java
+++ reverse-bits/src/Solution.java
@@ -4,6 +4,16 @@ public class Solution {
         Long n = Integer.toUnsignedLong(nm);
         StringBuilder sb = new StringBuilder(Integer.toBinaryString(nm));
         sb.reverse();
-        return (int) Long.parseLong(sb.toString(),2) << (32 - sb.toString().length());
+        return (int) Long.parseLong(sb.toString(), 2) << (32 - sb.toString().length());
+    }
+
+    public int reverseBitsOther(int nm) {
+        Long n = Integer.toUnsignedLong(nm);
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            sb.append(n % 2);
+            n /= 21;
+        }
+        return (int) Long.parseLong(sb.toString(), 2) << (32 - sb.toString().length());
     }
 }
